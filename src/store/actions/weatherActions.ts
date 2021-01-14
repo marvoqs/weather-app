7bb1c0a960d6e8b168d0eb03c2f53fa62a68bef4
marvoqs <marvoqs@gmail.com>
2021-01-14 14:40:24 +0100

    rename env variable to start with REACT_APP_

--- src/store/actions/weatherActions.ts
+++ src/store/actions/weatherActions.ts
@@ -5,7 +5,7 @@ import { WeatherAction, WeatherData, WeatherError, GET_WEATHER, SET_LOADING, SET
 export const getWeather = (city: string): ThunkAction<void, RootState, null, WeatherAction> => {
   return async (dispatch) => {
     try {
-      const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.WEATHER_API_KEY}`);
+      const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.REACT_APP_API_KEY}`);
 
       if (!res.ok) {
         const resData: WeatherError = await res.json();
